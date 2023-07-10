ad5465eb6ae980b352b57df4cec49a593d887fea
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2023-07-10 14:16:47 +0200

    remove product from cart test

--- cypress/e2e/cartTest.cy.js
+++ cypress/e2e/cartTest.cy.js
@@ -1,16 +1,23 @@
 import HomePage from "../page-objects/homePage";
 import CartPage from "../page-objects/cartPage";
 import { faker } from "@faker-js/faker";
+import { beforeEach } from "mocha";
 
 describe("cart page tests", () => {
   const homePage = new HomePage();
   const cartPage = new CartPage();
-
-  it("should add product to cart and delete it from there", function () {
+  beforeEach(() => {
     homePage.visitPage();
     homePage.addProductToCart();
+  });
+
+  it("should add product to cart", function () {
     homePage.clickGoToCartFromProductButton();
     cartPage.checkThatAddedProductIsInCart();
+  });
+
+  it("should remove product from cart", function () {
+    homePage.clickGoToCartFromProductButton();
     cartPage.removeItemFromCart();
     cartPage.checkThatCartIsEmpty();
   });

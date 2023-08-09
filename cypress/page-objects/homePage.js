bf85c5985d319d656cab6ac8f7b082417e462c65
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2023-08-09 12:54:29 +0200

    added my account page tests

--- cypress/page-objects/homePage.js
+++ cypress/page-objects/homePage.js
@@ -4,21 +4,6 @@ const myAccountHeaderButton = "#menu-item-100";
 const dataBlockNameNew = 'div[data-block-name="woocommerce/product-new"]';
 const checkCartFromProductLevel = ".added_to_cart.wc-forward";
 
-/*export const Product = {
-  HoodieWithZipper: {
-    Locator: 'a[data-product_id="51"]',
-    Name: "Hoodie with Zipper",
-  },
-  Polo: {
-    Locator: 'a[data-product_id="53"]',
-    Name: "Polo",
-  },
-  Sunglasses: {
-    Locator: 'a[data-product_id="49"]',
-    Name: "Sunglasses",
-  },
-};*/
-
 class HomePage {
   clickMyAccountHeaderButton() {
     cy.get(myAccountHeaderButton).click();
